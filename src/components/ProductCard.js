b785e00f1eaf2262302640dda61d1b94b53db425
Isaac <isaacforschool05@gmail.com>
2023-05-08 14:16:15 +0800

    able to conditionally render images, need to change to carousel

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,20 +2,27 @@ import React from "react";
 
 
 export default function ProductCard(props) {
-
-    console.log(props.variants);
     return (<>
         <a>
-            <div >
+            <div>
                 <div>
-                    {/* insert image here */}
+                    {props.variants && props.variants.length > 0 ? (
+                        props.variants.map((variant, index) => (
+                            <img
+                                key={index}
+                                src={variant.image_url}
+                                alt={variant.name}
+                                width="100%"
+                            />
+                        ))
+                    ) : (
+                        <p>No variants available</p>
+                    )}
                 </div>
                 <div>
                     <h3>{props.product.name}</h3>
                 </div>
             </div>
-
         </a>
-
     </>)
 }
