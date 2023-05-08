cb337810fb33356fec1d0324cc617c8fd4ca33e2
Isaac <isaacforschool05@gmail.com>
2023-05-08 22:13:06 +0800

    able to display both lures and variant details of lure by lureid

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -5,18 +5,31 @@ import LureContext from "../contexts/LureContext";
 
 export default function ProductPage() {
     const { lure_id } = useParams();
-    const [lure, setLure] = useState({})
+    const [lure, setLure] = useState({});
+    const [variants, setVariants] = useState([]);
 
     const context = useContext(LureContext);
     useEffect(() => {
         const getLure = async () => {
-            let lure = await context.getLureById(lure_id);
+            const lure = await context.getLureById(lure_id);
             setLure(lure);
         }
-        getLure(context, lure_id);
+        getLure();
+    }, [context, lure_id]);
+
+    useEffect(() => {
+        const getLureVariants = async () => {
+            const variants = await context.getAllVariantsByLureId(lure_id);
+            setVariants(variants);
+        }
+        getLureVariants();
     }, [context, lure_id])
 
     return (<React.Fragment>
         <h1>Welcome to product page of {lure.name} </h1>
+        <h3>
+            Basic Colour Display:
+            {variants.map(v => <p>{v.colour.name}</p>)}
+        </h3>
     </React.Fragment>)
 }
