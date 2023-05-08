01ef3fc42cdd1312f31daa6aadd2844aa6568fa8
Isaac <isaacforschool05@gmail.com>
2023-05-08 21:24:22 +0800

    managed to display prices and amount of variants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ProductListing from "./pages/ProductListing";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LureProvider from "./providers/LureProvider";
 import "./index.css"
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,28 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 export default function ProductCard(props) {
+
     return (<>
-        <a>
-            <div>
-                <div>
+        <Link>
+            <div className="shadow">
+                <div className="">
                     {props.variants && props.variants.length > 0 ? (
-                        props.variants.map((variant, index) => (
-                            <img
-                                key={index}
-                                src={variant.image_url}
-                                alt={variant.name}
-                                width="100%"
-                            />
-                        ))
+                        <img className="h-[75px]"src={props.variants[0].image_url} alt="" />
                     ) : (
                         <p>No variants available</p>
                     )}
                 </div>
                 <div>
                     <h3>{props.product.name}</h3>
+                    <div className="text-gray-900 flex items-center justify-between">
+                        <p>${(Math.min.apply(Math, props.variants.map(v => v.cost)))/100} - ${(Math.max.apply(Math, props.variants.map(v => v.cost)))/100} </p>
+                    {props.variants && props.variants.length > 0 ? (
+                        <p className="text-xs uppercase mr-1">{props.variants.length} variants</p>
+                    ) : (
+                        <p>No variants available</p>
+                    )}
+                    </div>
                 </div>
             </div>
-        </a>
+        </Link>
     </>)
 }
