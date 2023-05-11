5d8011a9407dc27acdfb852665409d539af96b7a
Isaac <isaacforschool05@gmail.com>
2023-05-11 18:26:44 +0800

    able to display all useful text on product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -30,34 +30,69 @@ export default function ProductPage() {
     const changeDisplay = (variantId) => {
         const newDisplay = variants.filter(variant => variant.id === variantId);
         setDisplayedLure(newDisplay[0]);
+        console.log(displayedLure);
     }
 
 
     return (<React.Fragment>
         {displayedLure && variants.length > 0 ?
             <section>
-                <div className="flex flex-col h-2/6 r">
+                <div className="flex flex-col items-center h-2/6">
                     <div>
                         <h1 className="text-3xl text-bold p-4">{lure.name}</h1>
                     </div>
-                    <div>
-                        <img className="w-1/2 h-[200px]" src={displayedLure.image_url} alt="" />
+                    <div className="shadow-md rounded-lg">
+                        <img className="w-full h-[200px]" src={displayedLure.image_url} alt="" />
                     </div>
                 </div>
-                <div className="flex">
+                <div className="flex justify-center mt-3">
                     {variants.map(variant => {
                         if (variant.colour.name === displayedLure.colour.name) {
                             return (
                                 <button
+                                    key={variant.id}
                                     className="text-xs p-2 m-2 border rounded-3xl bg-yellow-400 " >{variant.colour.name}</button>
                             );
                         };
                         return (
                             <button
+                                key={variant.id}
                                 onClick={() => changeDisplay(variant.id)}
                                 className="text-xs p-2 m-2 border rounded-3xl hover:bg-yellow-400 " >{variant.colour.name}</button>
                         )
                     })}
+                </div>
+                <div>
+                    <div className="px-10 mt-4">
+                        {lure.description}
+                    </div>
+                    <div className="flex">
+                        <div className="flex flex-col items-end px-20">
+                            <div className="border-2 p-4 rounded-md">
+                                Cost: ${displayedLure.cost / 100}
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-start px-10 mr-10">
+                            <div className="border-2 p-4 rounded-md">
+                                Specifications:
+                                <div className="text-sm">
+                                    Depth: {lure.depth === "0" ? "All" : lure.depth}
+                                </div>
+                                <div className="text-sm">
+                                    Hook: {lure.hook}
+                                </div>
+                                <div className="text-sm">
+                                    Type: {lure.type}
+                                </div>
+                                <div className="text-sm">
+                                    Size: {lure.size}mm
+                                </div>
+                                <div className="text-sm">
+                                    Weight: {lure.weight}g
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
             : <p>Loading...</p>}
