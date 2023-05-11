1e05d798b9ab488e72920af971b80c39a8d0b5f1
Isaac <isaacforschool05@gmail.com>
2023-05-11 17:41:55 +0800

    able to conditionally render variant details based on what the state is

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -25,19 +25,39 @@ export default function ProductPage() {
             setDisplayedLure(variants[0]);
         }
         getLureVariants();
-    }, [context, lure_id])
+    }, [context, lure_id]);
+
+    const changeDisplay = (variantId) => {
+        const newDisplay = variants.filter(variant => variant.id === variantId);
+        setDisplayedLure(newDisplay[0]);
+    }
 
 
     return (<React.Fragment>
         {displayedLure && variants.length > 0 ?
-            <section className="flex h-screen items-center">
-                <div className="">
+            <section>
+                <div className="flex flex-col h-2/6 r">
                     <div>
                         <h1 className="text-3xl text-bold p-4">{lure.name}</h1>
                     </div>
                     <div>
-                        <img className="w-1/2" src={displayedLure.image_url} alt="" />
+                        <img className="w-1/2 h-[200px]" src={displayedLure.image_url} alt="" />
                     </div>
+                </div>
+                <div className="flex">
+                    {variants.map(variant => {
+                        if (variant.colour.name === displayedLure.colour.name) {
+                            return (
+                                <button
+                                    className="text-xs p-2 m-2 border rounded-3xl bg-yellow-400 " >{variant.colour.name}</button>
+                            );
+                        };
+                        return (
+                            <button
+                                onClick={() => changeDisplay(variant.id)}
+                                className="text-xs p-2 m-2 border rounded-3xl hover:bg-yellow-400 " >{variant.colour.name}</button>
+                        )
+                    })}
                 </div>
             </section>
             : <p>Loading...</p>}
