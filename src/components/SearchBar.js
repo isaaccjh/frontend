4bd74f1b7a29ce268a66d3365cba9844015d6f18
Isaac <isaacforschool05@gmail.com>
2023-05-14 09:44:43 +0800

    filter button works, can change state onclick

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,14 +4,21 @@ import { useState } from "react";
 export default function SearchBar(props) {
     const [filter, setFilter] = useState(false);
 
+    const toggleFilter = () => {
+        setFilter(!filter);
+    }
+
     return (<>
         <div className="flex mt-2">
-            <div className="flex border-2 mx-3 px-4 items-center">
-                <button>Filter</button>
+            <div className="flex border-2 ml-0 mx-3 px-4 items-center">
+                <button className="text-xs" onClick={toggleFilter}>Filter & Sort</button>
             </div>
             <div className="flex w-3/4">
                 <input className="w-3/4" type="text" />
             </div>
+        </div>
+        <div>
+            {filter ? <p>Filter has been toggled</p> : null}
         </div>
     </>)
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -43,25 +43,25 @@ export default function Profile() {
                     <div>Contact Number:{userDetails?.contact_number ? userDetails?.contact_number : null}</div>
                     <div>Email: {userDetails?.email }</div>
                 </div>
-            <h1 className="text-xl text-bold">Orders</h1>
+            <h1 className="text-xl text-bold p-3 pl-0">Orders</h1>
             {userOrders && userOrders.length !== 0 ?
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                     <table className="w-full text-xs text-left text-gray-700">
                         <thead className="text-[10px] text-gray-900 uppercase bg-gray-50">
                             <tr>
-                                <th scope="col" class="px-4 py-2">Order ID</th>
-                                <th scope="col" class="px-4 py-2">Shipping Address</th>
-                                <th scope="col" class="px-4 py-2">Total Cost</th>
-                                <th scope="col" class="px-4 py-2">Payment Type</th>
-                                <th scope="col" class="px-4 py-2">Order Status</th>
-                                <th scope="col" class="px-4 py-2">Order Date</th>
-                                <th scope="col" class="px-4 py-2"></th>
+                                <th className="px-4 py-2">Order ID</th>
+                                <th className="px-4 py-2">Shipping Address</th>
+                                <th className="px-4 py-2">Total Cost</th>
+                                <th className="px-4 py-2">Payment Type</th>
+                                <th className="px-4 py-2">Order Status</th>
+                                <th className="px-4 py-2">Order Date</th>
+                                <th className="px-4 py-2"></th>
                             </tr>
                         </thead>
                         <tbody>
                             {userOrders.map(order => {
                                 return (
-                                    <tr key={order.id} scope="row" className="bg-white border-b">
+                                    <tr key={order.id} className="bg-white border-b">
                                         <td className="px-4 py-2">{order.id}</td>
                                         <td className="px-4 py-2">{order.shipping_address_line_1}{order.shipping_address_line_2}</td>
                                         <td className="px-4 py-2">${order.total_cost / 100}</td>
