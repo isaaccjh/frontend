cf062e92cf202dbd26d62fc9b6be283989edef1a
Isaac <isaacforschool05@gmail.com>
2023-05-14 16:43:37 +0800

    login page done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,32 +34,32 @@ export default function Navbar() {
 
 
     return (<>
-        <header aria-label="Site Header" className="border-b border-gray-100">
+        <header className="border-b border-gray-100 bg-yellow-400">
             <div
-                className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8"
+                className="mx-auto shadow flex h-16 max-w-screen-2xl items-center justify-between sm:px-6 lg:px-8"
             >
                 <div className="flex items-center gap-4 p-2">
                     <button onClick={toggleHamburger} type="button" className="p-2 border-2 rounded-lg lg:hidden" >
                         <RxHamburgerMenu className="h-6 w-6" />
                     </button>
 
-                    <Link href="#" className="flex">
+                    <Link to="/home" className="flex">
                         <span className="sr-only">Logo</span>
                         <span className="inline-block h-10 w-32 rounded-lg bg-gray-200"></span>
                     </Link>
                 </div>
 
                 <div className="flex flex-1 items-center justify-end gap-8">
-                    <nav aria-label="Site Nav" className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500">
-                        <Link href="/about" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-yellow-400">
-                            About
+                    <nav className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-[#252525]">
+                        <Link href="/home" className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-yellow-400">
+                            Home
                         </Link>
 
                         <Link
-                            href="/news"
+                            href="/about"
                             className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-yellow-400"
                         >
-                            News
+                            About
                         </Link>
 
                         <Link
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,12 +5,16 @@ import "../index.css";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+import { MdOutlineAlternateEmail } from "react-icons/md"
+import { IoIosEyeOff, IoIosEye } from "react-icons/io"
+
 import UserContext from "../contexts/UserContext";
 import { checkPassword } from "../validator/password";
 
 export default function Login() {
     const navigate = useNavigate();
     let context = useContext(UserContext);
+    const [passwordVisibility, setPasswordVisibility] = useState(false);
     const [formState, setFormState] = useState({
         "email": "",
         "password": ""
@@ -21,6 +25,10 @@ export default function Login() {
             ...formState,
             [e.target.name]: e.target.value
         })
+    }
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisibility(!passwordVisibility);
     }
 
     const submitForm = async () => {
@@ -44,28 +52,72 @@ export default function Login() {
     }
 
     return (<>
-        <h1>Login</h1>
-        <div>
-            <label>Email:</label>
-            <input
-                type="email"
-                className="ml-2"
-                name="email"
-                value={formState.email}
-                onChange={updateFormField}
-            />
-        </div>
-        <div>
-            <label>Password:</label>
-            <input
-                type="password"
-                className="ml-2"
-                name="password"
-                value={formState.password}
-                onChange={updateFormField}
-            />
-        </div>
-        <ToastContainer />
-        <button type="submit" onClick={submitForm} className=" bg-blue-600">Login</button>
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-lg">
+                <h1 className="text-center text-2xl font-bold text-[#252525] sm:text-3xl">
+                    Join Us today!
+                </h1>
+
+                <p className="mx-auto mt-4 max-w-md text-center text-[#252525]">
+                    Whether you're a seasoned angler or just dipping your toes into the world of fishing, you've arrived at the perfect destination for all your lure needs.
+                </p>
+
+                <div
+                    action=""
+                    className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+                >
+                    <p className="text-center text-[#252525] text-lg font-medium">Login to your account</p>
+
+                    <div>
+                        <label className="sr-only">Email</label>
+                        <div className="relative">
+                            <input
+                                onChange={updateFormField}
+                                value={formState.email}
+                                name="email"
+                                type="email"
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Enter email"
+                            />
+
+                            <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                                <MdOutlineAlternateEmail />
+                            </span>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="sr-only">Password</label>
+
+                        <div className="relative">
+                            <input
+                                onChange={updateFormField}
+                                value={formState.password}
+                                name="password"
+                                type={passwordVisibility ? "text" : "password"}
+                                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                                placeholder="Enter password"
+                            />
+
+                            <button onClick={togglePasswordVisibility} className="absolute inset-y-0 border-gray-200 end-0 grid place-content-center px-4">
+                                {passwordVisibility ? <IoIosEyeOff /> : <IoIosEye />}
+                            </button>
+                        </div>
+                    </div>
+
+                    <button
+                        onClick={submitForm}
+                        className="block w-full rounded-lg bg-yellow-200 hover:bg-yellow-400 px-5 py-3 text-sm font-medium text-[#252525]"
+                    >
+                        Login
+                    </button>
+
+                    <p className="text-center text-sm text-gray-800">
+                        No account?
+                        <a className="underline text-sm ml-2 hover:text-blue-500" href="">Sign up</a>
+                    </p>
+                </div>
+            </div>
+        </div >
     </>)
 }
